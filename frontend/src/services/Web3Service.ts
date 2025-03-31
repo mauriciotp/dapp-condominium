fcf96acecc28fdf22f3656bf82cb943a11fdde9b
mauricio <mauricio.tp_@outlook.com>
2025-03-30 21:01:01 -0300

    feat: verify profile type

--- frontend/src/services/Web3Service.ts
+++ frontend/src/services/Web3Service.ts
@@ -1,19 +1,48 @@
-import { createWalletClient, custom } from 'viem'
-import { sepolia } from 'viem/chains'
 import 'viem/window'
+import { createWalletClient, custom, getContract } from 'viem'
+import { sepolia } from 'viem/chains'
+import { abi } from './abi'
 
-export const client = createWalletClient({
+const ADAPTER_ADDRESS = import.meta.env.VITE_ADAPTER_ADDRESS as `0x${string}`
+
+export enum Profile {
+  RESIDENT = 0,
+  COUNSELOR = 1,
+  MANAGER = 2,
+}
+
+const client = createWalletClient({
   chain: sepolia,
   transport: custom(window.ethereum!),
 })
 
-export async function doLogin() {
+const contract = getContract({
+  abi,
+  address: ADAPTER_ADDRESS,
+  client,
+})
+
+export type LoginResult = {
+  account: string
+  profile: Profile
+}
+
+export async function doLogin(): Promise<LoginResult> {
   const accounts = await client.requestAddresses()
 
   if (!accounts || !accounts.length)
     throw new Error('Wallet not found/allowed.')
 
+  const manager = await contract.read.getManager()
+  const isManager = manager === accounts[0]
+
+  if (isManager) localStorage.setItem('profile', `${Profile.MANAGER}`)
+  else localStorage.setItem('profile', `${Profile.RESIDENT}`)
+
   localStorage.setItem('account', accounts[0])
 
-  return accounts[0]
+  return {
+    account: accounts[0],
+    profile: parseInt(localStorage.getItem('profile') || '0'),
+  }
 }
